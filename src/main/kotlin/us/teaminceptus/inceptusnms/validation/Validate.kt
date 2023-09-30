fbd1080edff5e3e0a462b9b18e33f0534644c231
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2023-09-29 22:22:52 -0500

    Update Validate.kt

--- src/main/kotlin/us/teaminceptus/inceptusnms/validation/Validate.kt
+++ src/main/kotlin/us/teaminceptus/inceptusnms/validation/Validate.kt
@@ -4,28 +4,35 @@ package us.teaminceptus.inceptusnms.validation
 
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.*
+import us.teaminceptus.inceptusnms.generation.Util.error
 import us.teaminceptus.inceptusnms.generation.Util.getJavaName
 import us.teaminceptus.inceptusnms.generation.Util.getJavaPackage
+import us.teaminceptus.inceptusnms.generation.Util.log
 import java.io.File
+import java.lang.AssertionError
 
 fun main(args: Array<String>) {
-    if (args.size != 1) {
-        println("Usage: ./gradlew validate -Parg1=<input docs folder>")
-        return
-    }
-
-    val input = File(args[0])
-    assert(input.exists()) { "Input documentation folder does not exist!" }
-
-    val files = input.walkTopDown().filter { it.name.contains(".json") }
-    files.forEach { file ->
-        try {
-            val json = Json.parseToJsonElement(file.readText()).jsonObject
-
-            validateClass(file, json)
-        } catch (e: SerializationException) {
-            throw IllegalStateException("Invalid JSON File: ${file.absolutePath}", e)
+    try {
+        if (args.size != 1) {
+            error("Usage: ./gradlew validate -Parg1=<input docs folder>")
+            return
         }
+
+        val input = File(args[0])
+        assert(input.exists()) { "Input documentation folder does not exist!" }
+
+        val files = input.walkTopDown().filter { it.name.contains(".json") }
+        files.forEach { file ->
+            try {
+                val json = Json.parseToJsonElement(file.readText()).jsonObject
+
+                validateClass(file, json)
+            } catch (e: SerializationException) {
+                error("Invalid JSON File: ${file.absolutePath}")
+            }
+        }
+    } catch (e: AssertionError) {
+        error(e.message ?: "Assertion Error")
     }
 }
 
@@ -41,5 +48,5 @@ fun validateClass(file: File, json: JsonObject) {
     if (classType == "interface")
         assert(!json.contains("constructors")) { "Interface cannot have constructors!" }
 
-    println("Class ${getJavaName(file)} validated")
+    log("Class ${getJavaName(file)} validated")
 }
